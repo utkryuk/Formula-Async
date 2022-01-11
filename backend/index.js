6fdfe7c54dc66df3c350b96efc7d91b52389936b
utkryuk <utkryuk@gmail.com>
2022-01-11 23:52:20 +0530

    initial backend working

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', (request, response) => {
+  response.send('<h1>Backend working</h1>')
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
