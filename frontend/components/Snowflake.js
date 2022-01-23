94f662592b4000b27e9837cd665f93d8ad905c66
puns2678 <shreyansh2678@gmail.com>
2022-01-24 02:21:54 +0530

    good job

--- frontend/components/Snowflake.js
+++ frontend/components/Snowflake.js
@@ -1,41 +1,41 @@
 import React from 'react';
 import styles from '../styles/Home.module.css'
 const Snowflake = () => {
-  return <div class = {styles.snowflakes}>
-  <div class={styles.snowflake}>
+  return <div className = {styles.snowflakes}>
+  <div className={styles.snowflake}>
   ❅
   </div>
-  <div class={styles.snowflake}>
+  <div className={styles.snowflake}>
   ❆
   </div>
-  <div class={styles.snowflake}>
+  <div className={styles.snowflake}>
   ❅
   </div>
-  <div class={styles.snowflake}>
+  <div className={styles.snowflake}>
   ❆
   </div>
-  <div class={styles.snowflake}>
+  <div className={styles.snowflake}>
   ❅
   </div>
-  <div class={styles.snowflake}>
+  <div className={styles.snowflake}>
   ❆
   </div>
-  <div class={styles.snowflake}>
+  <div className={styles.snowflake}>
     ❅
   </div>
-  <div class={styles.snowflake}>
+  <div className={styles.snowflake}>
     ❆
   </div>
-  <div class={styles.snowflake}>
+  <div className={styles.snowflake}>
     ❅
   </div>
-  <div class={styles.snowflake}>
+  <div className={styles.snowflake}>
     ❆
   </div>
-  <div class={styles.snowflake}>
+  <div className={styles.snowflake}>
     ❅
   </div>
-  <div class={styles.snowflake}>
+  <div className={styles.snowflake}>
     ❆
   </div>
 </div>
